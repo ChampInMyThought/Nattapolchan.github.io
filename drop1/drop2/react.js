6948f90a10a456925cc2e6cc9212c08f9421f691
NattapolChan <66078943+NattapolChan@users.noreply.github.com>
2020-10-04 16:12:58 +0700

    m

--- drop1/drop2/react.js
+++ drop1/drop2/react.js
@@ -1,4 +1,5 @@
+
 const tl = gsap.timeline({defaults: {ease:"power1.out"}});
 tl.to(".text",{ y: "0%", duration: 1, stagger: 0.25});
-tl.to(".slidercover",{ y:"0%" , duration: 1.5,delay: 0.5});
-tl.to(".maincontent",{ y:"0%" , duration: 1.5,delay: 0.6});
+tl.to(".slider",{ y:"0%" , duration: 1.5,delay: 0.5});
+tl.to(".slider",{ y:"-100%" , duration: 1.5,delay: 0.6});
